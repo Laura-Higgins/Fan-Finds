ea70632cd2404fe077f5c891c9765663cdd76c1f
Laura Higgins <lauraghiggins@me.com>
2017-04-11 14:52:52 -0700

    updated issues.

--- issue1/index.js
+++ issue1/index.js
@@ -6,14 +6,22 @@ app.listen(3000, () => {
   console.log('listening on port 3000!')
 })
 
-app.request('https://jsonplaceholder.typicode.com/users', function(error, body){
-  if(error) {
-    console.log('error!')
-  }
-  else {
-    console.log(body)
-  }
+var getInsta = app.get('/social', function(req, res, next) {
+  var promise = new Promise((resolve, reject) => {
+    request('https://api.instagram.com/v1/users/6479d1bb7b75466ba3dc51c1f1e706e8/media/recent/?access_token=11234493.6479d1b.1573104f80c845e59639b10d51db011e', (error, res, body) => {
+      if(error) {
+        reject(error)
+      }
+      else {
+        resolve(body)
+      }
+    })
+  })
+  return promise
 })
 
+getInsta.then(function(){
+  console.log(promise)
+})
 
 // 'https://api.instagram.com/v1/users/6479d1bb7b75466ba3dc51c1f1e706e8/media/recent/?access_token=11234493.6479d1b.1573104f80c845e59639b10d51db011e'
