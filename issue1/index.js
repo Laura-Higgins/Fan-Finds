baece1da5b82be7b054d6368274109993d19ae24
Laura Higgins <lauraghiggins@me.com>
2017-04-11 15:32:17 -0700

    corrected request.

--- issue1/index.js
+++ issue1/index.js
@@ -6,22 +6,16 @@ app.listen(3000, () => {
   console.log('listening on port 3000!')
 })
 
-var getInsta = app.get('/social', function(req, res, next) {
-  var promise = new Promise((resolve, reject) => {
+app.get('/social', function(req, response, next) {
     request('https://api.instagram.com/v1/users/6479d1bb7b75466ba3dc51c1f1e706e8/media/recent/?access_token=11234493.6479d1b.1573104f80c845e59639b10d51db011e', (error, res, body) => {
-      if(error) {
-        reject(error)
+      if(error){
+        console.log(error)
       }
       else {
-        resolve(body)
+        response.json(body)
       }
     })
-  })
-  return promise
 })
 
-getInsta.then(function(){
-  console.log(promise)
-})
 
 // 'https://api.instagram.com/v1/users/6479d1bb7b75466ba3dc51c1f1e706e8/media/recent/?access_token=11234493.6479d1b.1573104f80c845e59639b10d51db011e'
