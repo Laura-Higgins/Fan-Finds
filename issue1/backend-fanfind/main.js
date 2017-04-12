895f1652bee65ba922ca49725a16fe4571d777a1
Laura Higgins <lauraghiggins@me.com>
2017-04-12 13:20:21 -0700

    installed body parser.

--- issue1/backend-fanfind/main.js
+++ issue1/backend-fanfind/main.js
@@ -1,16 +1,19 @@
 var Twitter = require('twitter');
 var twitterCred = require('/twitter.json')
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 var client = new Twitter(twitterCred);
 
 var params = {q: 'Nikki Tutorials'}; //querying html input
 
 client.get('search/tweets', params, function(error, tweets, response) {
-  tweets = eval(tweets.statuses) //TODO use body parser
+  tweets = tweets.statuses
   if (!error) {
     tweets.forEach((tweet) => {
-      console.log(tweet.text + "-----------------------")
+      console.log(tweet.text + "+++++++++")
     })
-    // console.log(tweets);
   }
 });
