f1bb17456d4335b06034ba7e6fa07d552bd957b3
Laura Higgins <lauraghiggins@me.com>
2017-04-12 09:54:11 -0700

    working with GET tweets.

--- issue1/index.js
+++ issue1/index.js
@@ -1,21 +1,30 @@
-var express = require('express')
-var app = express()
-var request = require('request')
+var Twitter = require('twitter');
 
-app.listen(3000, () => {
-  console.log('listening on port 3000!')
-})
+var client = new Twitter({
+  consumer_key: 'H6yThmMbDKIdm0rNTGUi3GIit',
+  consumer_secret: 'rSE2eB9NrrzZ1mldqwUuwu4ebNLQYHh2d79y8m0SJ6LhMcoI7i',
+  access_token_key: '33303861-deUD9hncG37AEwIdhgnP5iVAVW87ZbF5QIoFbu56q',
+  access_token_secret: 'o81cNCkzDdBcazYAcFBJ8UKthcLTHudMt0DLbvAXfeC5K'
+});
 
-app.get('/social', function(req, response, next) {
-    request('https://api.instagram.com/v1/users/6479d1bb7b75466ba3dc51c1f1e706e8/media/recent/?access_token=11234493.6479d1b.1573104f80c845e59639b10d51db011e', (error, res, body) => {
-      if(error){
-        console.log(error)
-      }
-      else {
-        response.json(body)
-      }
+var params = {q: 'John Kuckian'}; //querying html input
+
+client.get('search/tweets', params, function(error, tweets, response) {
+  tweets = eval(tweets.statuses) //TODO use body parser
+  if (!error) {
+    tweets.forEach((tweet) => {
+      console.log(tweet.text + "-----------------------")
     })
-})
+    // console.log(tweets);
+  }
+});
 
 
-// 'https://api.instagram.com/v1/users/6479d1bb7b75466ba3dc51c1f1e706e8/media/recent/?access_token=11234493.6479d1b.1573104f80c845e59639b10d51db011e'
+
+// var express = require('express')
+// var app = express()
+// var request = require('request')
+//
+// app.listen(3000, () => {
+//   console.log('listening on port 3000!')
+// })
