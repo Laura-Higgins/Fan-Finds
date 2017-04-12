6990af642c0c2e0fd5b03bc46a365aade7c8b5ae
Laura Higgins <lauraghiggins@me.com>
2017-04-12 14:17:55 -0700

    Corrected Tree of directories, created Public folder.

---
+++ index.js
@@ -0,0 +1,23 @@
+var Twitter = require('twitter');
+var twitterCred = require('./twitter.json')
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+var client = new Twitter(twitterCred);
+
+var params = {q: 'Nikki Tutorials'}; //querying html input
+
+client.get('search/tweets', params, function(error, tweets, response) {
+  tweets = tweets.statuses
+  if (!error) {
+    tweets.forEach((tweet) => {
+      console.log("++++++" + tweet.text + "+++++++++")
+    })
+  }
+});
+
+app.listen(3000, () => {
+  console.log('listening port 3000!')
+})
